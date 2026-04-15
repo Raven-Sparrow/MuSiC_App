9894a93c64f2d49879d3c797baf3ebba2b2bf404
Akash <makash20067@gmail.com>
2026-04-15 10:57:02 +0530

    fix: update webDir to dist for cloud build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.akash.auraplayer',
   appName: 'AuraPlayer',
-  webDir: 'public'
+  webDir: 'dist'
 };
 
 export default config;
